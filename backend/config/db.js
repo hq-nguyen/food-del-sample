3fca12595e3b215319ae5d1cbba229d725de064e
Hai-Quan Nguyen <136264412+hq-nguyen@users.noreply.github.com>
2024-11-12 16:03:10 +0000

    first commit

--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://greatstack:02013003@cluster0.taqdu.mongodb.net/food-del').then(()=>console.log("DB connected"));
-
-}
